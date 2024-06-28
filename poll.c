5201430889046ef7a082b44f34d14fca5224c856
ikunkunkunku <15633467139@163.com>
2024-06-28 09:48:45 +0800

    pool.c

--- poll.c
+++ poll.c
@@ -36,6 +36,7 @@ typedef struct{
 	int shutdown; //1关闭 0 
 }threadpool_t;
 
+//任务处理函数
 static void *threadpool_job(void *arg)
 {
 	threadpool_t *pool = arg;
@@ -48,7 +49,7 @@ static void *threadpool_job(void *arg)
 		{
 			pthread_cond_wait(&pool->task_queue_not_empty, &pool->lock);
 
-			//是否
+			//是否由于线程终止的通知
 			if(pool->wait_exit_thr_num > 0)
 			{
 				pool->wait_exit_thr_num--;
@@ -68,11 +69,79 @@ static void *threadpool_job(void *arg)
 			pthread_exit(NULL);
 		}
 		//任务来了
+		queue_deq(pool->task_queue, &task);
+		pool->queue_size--;
+		pthread_cond_broadcast(&pool->task_queue_not_full);
+		pthread_mutex_unlock(&pool->lock);
 
+		pthread_mutex_lock(&pool->busy_lock);
+		pool->busy_thr_num++;
+		pthread_mutex_unlock(&pool->busy_lock);
 
+		//执行任务
+		(task.job)(task.arg);
+		
+		pthread_mutex_lock(&pool->busy_lock);
+		pool->busy_thr_num--;
+		pthread_mutex_unlock(&pool->busy_lock);
 	}
 }
 
+//管理者线程
+static void * admin_thread(void *arg)
+{
+	threadpool_t *pool = arg;
+	int queue_size, live_thr_num, busy_thr_num, min_thr_num;
+	int pos;
+	while(!pool->shutdown)
+	{
+		pthread_mutex_lock(&pool->lock);
+		queue_size = pool->queue_size;
+		live_thr_num = pool->live_thr_num;
+		min_thr_num = pool->min_thr_num;
+		pthread_mutex_unlock(&pool->lock);
+		
+		pthread_mutex_lock(&pool->busy_lock);
+		busy_thr_num = pool->busy_thr_num;
+		pthread_mutex_unlock(&pool->busy_lock);
+
+		//空线程5
+		if(queue_size >= MIN_FREE_THR && pool->cur_live_thr_num < \
+				pool->max_thr_num)
+		{
+			pthread_mutex_lock(&pool->lock);
+			
+			for(int i = 0, pos = 0;i < DEFAULT_ADD_THR_NUM &&\
+					pool->live_thr_num < pool->max_thr_num && \
+					pos < pool->max_thr_num; pos++)
+			{
+				if((pool->threads[pos] != 0 && \
+							thread_alive((pool->threads)[pos])))
+					continue;
+				pthread_create((pool->threads) + pos, NULL, threadpool_job, \
+						(void *)pool);
+				i++;
+				pool->cur_live_thr_num++;
+			}
+			pthread_mutex_unlock(&pool->lock);
+		}
+
+		//销毁多余的线程
+		if(busy_thr_num *2 < live_thr_num && live_thr_num > min_thr_num)
+		{
+			pthread_mutex_lock(&pool->lock);
+			pool->wait_exit_thr_num = DEFAULT_DESTROY_NUM;
+			pthread_mutex_unlock(&pool->lock);
+
+			for(int i = 0; i < DEFAULT_DESTROY_NUM; i++)
+			{
+				pthread_cond_signal(&pool->task_queue_not_empty);
+			}
+		}
+		sleep(1);
+	}
+	pthread_exit((void *)0);
+}
 
 
 threadpool_t *threadpool_init(int queue_max_size, int min_thr_num, int max_thr_num)
@@ -142,8 +211,54 @@ threadpool_t *threadpool_init(int queue_max_size, int min_thr_num, int max_thr_n
 	return mypool;
 }
 //添加任务
-int threadpool_add_task(threadpool_t  *pool, job_t job, void *arg);
+int threadpool_add_task(threadpool_t  *pool, job_t job, void *arg)
+{
+	threadpool_task_t task;
+	pthread_mutex_lock(&pool->lock);
+	while(queue_isfull(pool->task_queue))
+	{
+		pthread_cond_wait(&pool->task_queue_not_full, &pool->lock);
+	}
+	if(pool->shutdown)
+	{
+		pthread_mutex_unlock(&pool->lock);
+		return -1;
+	}
+	task.job = job;
+	task.arg = arg;
+	queue_enq(pool->task_queue, &task);
+	pool->queue_size++;
+	pthread_cond_signal(&pool->task_queue_not_empty);
+	pthread_mutex_unlock(&pool->task_queue);
 
+	return 0;
+}
 //销毁
-void threadpool_destory(threadpool_t *pool);
+void threadpool_destory(threadpool_t *pool)
+{
+	pool->shutdown = 1;
+
+	//管理者收拾
+	pthread_join(pool->admin_tid, NULL);
+
+	for(int i = 0;i < pool->cur_live_thr_num; i++)
+		pthread_cond_broadcast(&pool->task_queue_not_empty);
+
+	for(int i = 0; i < pool->max_thr_num; i++)
+	{
+		if((pool->threads)[i] == 0)
+			continue ;
+		if(thread_alive(pool->threads)[i])
+			pthread_join((pool->threads)[i], NULL);
+	}
+	free(pool->threads);
+	queue_destroy(&pool->lock);
+	pthread_mutex_destroy(&pool->lock);
+	pthread_mutex_destroy(&pool->busy_lock);
+	pthread_cond_destroy(&pool->task_queue_not_full);
+	pthread_cond_destroy(&pool->task_queue_not_empty);
+
+	free(pool);
+	pool = NULL;
+}
 
